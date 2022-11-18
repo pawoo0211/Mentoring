fc18f6858963365fa7756638f50e8eac931da6b3
pawoo0211 <pawoo0211@naver.com>
2022-11-18 22:33:18 +0900

    [common] ResponseEntity 네이밍 변경 #13
    "ResponseEntity"에서 "CommonResponse"로 변경

--- src/main/java/com/example/mentoring/common/CommonResponse.java
+++ src/main/java/com/example/mentoring/common/CommonResponse.java
@@ -3,18 +3,20 @@ package com.example.mentoring.common;
 import lombok.Getter;
 
 @Getter
-public class ResponseEntity<T> {
+public class CommonResponse<T> {
 
     private Boolean isSuccess;
     private final T out;
 
-    public ResponseEntity(T out){
+    // 기존 소스코드, 생성자를 이용해서 응답 값 구별(true or false)
+    public CommonResponse(T out){
         this.isSuccess = true;
         this.out = out;
     }
 
-    public ResponseEntity(boolean isSuccess, T out){
+    public CommonResponse(boolean isSuccess, T out){
         this.isSuccess = isSuccess;
         this.out = out;
     }
+
 }
--- src/main/java/com/example/mentoring/controller/OrderController.java
+++ src/main/java/com/example/mentoring/controller/OrderController.java
@@ -1,6 +1,6 @@
 package com.example.mentoring.controller;
 
-import com.example.mentoring.common.ResponseEntity;
+import com.example.mentoring.common.CommonResponse;
 import com.example.mentoring.order.domain.OrderEntity;
 import com.example.mentoring.order.in.OrderIn;
 import com.example.mentoring.order.out.OrderOut;
@@ -19,7 +19,7 @@ public class OrderController {
     private final OrderService orderService;
 
     @PostMapping("/order")
-    public ResponseEntity placeOrder(@RequestBody @Valid OrderIn orderIn) {
+    public CommonResponse placeOrder(@RequestBody @Valid OrderIn orderIn) {
         OrderEntity orderEntity = orderService.placeOrder(orderIn);
 
         // 주문 DTO 생성, 주문 상태가 정상일 경우로 가정(중복코드 제거를 위해)
@@ -32,10 +32,10 @@ public class OrderController {
         // 비정상 응답 반환
         if (!orderEntity.isOrderState()) {
             orderOut.setOrderState(false);
-            return new ResponseEntity(false, orderOut);
+            return new CommonResponse(false, orderOut);
         }
 
         // 정상 응답 반환
-        return new ResponseEntity(orderOut);
+        return new CommonResponse(orderOut);
     }
 }
