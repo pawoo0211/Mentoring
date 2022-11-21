426422c9f197b7bafc649325e185e3f86e1b9491
pawoo0211 <pawoo0211@naver.com>
2022-11-21 20:29:45 +0900

    [Dto] "RegisterMenuIn" 역직렬화 오류 해결
    "@NoArgsConstructor" 이용

--- src/main/java/com/example/mentoring/menu/in/RegisterMenuIn.java
+++ src/main/java/com/example/mentoring/menu/in/RegisterMenuIn.java
@@ -1,11 +1,13 @@
 package com.example.mentoring.menu.in;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 
 @Getter
+@NoArgsConstructor // "cannot deserialize from Object value" 해결, JSON -> 객체 생성시 기본(Default) 생성자 필요
 public class RegisterMenuIn {
 
     @NotEmpty(message = "메뉴를 입력해주세요.")
