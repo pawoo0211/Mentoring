046ad7c3f44236dbbd9af207d25fb08eafe786f4
pawoo0211 <pawoo0211@naver.com>
2022-11-23 18:56:37 +0900

    add logging interceptor class

---
+++ src/main/java/com/example/mentoring/common/CustomInterceptor.java
@@ -0,0 +1,42 @@
+package com.example.mentoring.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.lang.Nullable;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 동기 "Interceptor" -> "HandlerInterceptor"
+ * 비동기 "Interceptor" -> "AsyncHandlerInterceptor"
+ */
+@Slf4j
+public class CustomInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse Response, Object handler)
+            throws Exception {
+
+        log.info("CustomInterceptor - preHandle");
+        log.info("Before calling the controller function");
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
+                           @Nullable ModelAndView modelAndView) throws Exception {
+
+        log.info("CustomInterceptor - postHandle");
+        log.info("After calling the controller function");
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
+                                @Nullable Exception ex) throws Exception {
+
+        log.info("CustomInterceptor - afterCompletion");
+        log.info("Method process completion");
+    }
+}
