961a1e384c2cdf8ad5db210c25d4dfbc02a112d5
mumblecoder <mumblecoder@gmail.com>
2022-12-07 20:55:34 +0900

    add ThreadLocalContextHolder

--- src/main/java/com/example/mentoring/common/ThreadLocalInterceptor.java
+++ src/main/java/com/example/mentoring/common/ThreadLocalInterceptor.java
@@ -1,5 +1,6 @@
 package com.example.mentoring.common;
 
+import com.example.mentoring.common.threadlocal.ThreadLocalContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.lang.Nullable;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -11,34 +12,16 @@ import javax.servlet.http.HttpServletResponse;
 @Slf4j
 public class ThreadLocalInterceptor implements HandlerInterceptor {
 
-    private static final ThreadLocal<HttpServletRequest> httpRequestThread = new ThreadLocal<>();
-    private static final ThreadLocal<HttpServletResponse> httpResponseThread = new ThreadLocal<>();
-
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
-            throws Exception {
-
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         log.info("ThreadInterceptor: Interceptor`s preHandle method is called");
-        httpRequestThread.set(request);
-        httpResponseThread.set(response);
         return true;
     }
 
     @Override
-    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
-                           @Nullable ModelAndView modelAndView) throws Exception {
-
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, @Nullable ModelAndView modelAndView) {
         log.info("ThreadInterceptor: Interceptor`s postHandle method is called");
-        log.info("Request`s URI: " + httpRequestThread.get().getRequestURI());
-        log.info("Response`s Status: " + httpResponseThread.get().getStatus());
+        ThreadLocalContextHolder.reset();
     }
 
-    @Override
-    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler,
-                                @Nullable Exception ex) throws Exception {
-
-        log.info("ThreadInterceptor: Interceptor`s afterCompletion method is called");
-        httpRequestThread.remove();
-        httpResponseThread.remove();
-    }
 }
