009753acf7a74f9a625a53f22129d1bb738362d1
pawoo0211 <pawoo0211@naver.com>
2022-12-24 10:45:21 +0900

    add MerchantRepository Interface #24

---
+++ src/main/java/com/example/mentoring/merchant/domain/MerchantRepository.java
@@ -0,0 +1,6 @@
+package com.example.mentoring.merchant.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MerchantRepository extends JpaRepository<MerchantEntity,Long> {
+}
