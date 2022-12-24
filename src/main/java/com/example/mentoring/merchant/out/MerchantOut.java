4f1736bc68d53a35bf1774f1d033d0e3e7ebe8d2
pawoo0211 <pawoo0211@naver.com>
2022-12-24 10:39:31 +0900

    add MerchantOut Class

--- src/main/java/com/example/mentoring/merchant/out/MerchantOut.java
+++ src/main/java/com/example/mentoring/merchant/out/MerchantOut.java
@@ -3,9 +3,12 @@ package com.example.mentoring.merchant.out;
 import lombok.Builder;
 import lombok.Getter;
 
+import javax.validation.constraints.NotEmpty;
+
 @Getter
 public class MerchantOut {
 
+    @NotEmpty(message = "가맹점 이름을 입력해주세요.")
     private String name;
     private Long merchantId;
 
