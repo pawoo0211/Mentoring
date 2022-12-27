e533734d2168a341d16e3d1a680d0d33df72e42a
pawoo0211 <pawoo0211@naver.com>
2022-12-27 20:13:13 +0900

    Add Component in MerchantNotFoundException #28

--- src/main/java/com/example/mentoring/exception/domain/MerchantNotFoundException.java
+++ src/main/java/com/example/mentoring/exception/domain/MerchantNotFoundException.java
@@ -1,10 +1,19 @@
 package com.example.mentoring.exception.domain;
 
-public class MerchantNotFoundException extends RuntimeException{
+import com.example.mentoring.constant.ExceptionKeyword;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MerchantNotFoundException extends MentoringException {
 
     private static final String MESSAGE = "해당 가맹점은 존재하지 않는 가맹점입니다.";
 
     public MerchantNotFoundException() {
         super(MESSAGE);
+    }
+
+    @Override
+    public String getKeyword() {
+        return ExceptionKeyword.Merchant.getKeyword();
     }
 }
