1c9e9e06ee5ea943e8ff8a16cfb5ca355953224a
pawoo0211 <pawoo0211@naver.com>
2022-12-27 20:12:53 +0900

    Add Component in MenuNotFoundException #28

--- src/main/java/com/example/mentoring/exception/domain/MenuNotFoundException.java
+++ src/main/java/com/example/mentoring/exception/domain/MenuNotFoundException.java
@@ -1,11 +1,20 @@
 package com.example.mentoring.exception.domain;
 
-public class MenuNotFoundException extends RuntimeException {
+import com.example.mentoring.constant.ExceptionKeyword;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MenuNotFoundException extends MentoringException {
 
     // log Message
     private static final String MESSAGE = "해당 메뉴는 존재하지 않는 메뉴입니다.";
 
     public MenuNotFoundException() {
-         super(MESSAGE);
+        super(MESSAGE);
      }
+
+    @Override
+    public String getKeyword(){
+        return ExceptionKeyword.Menu.getKeyword();
+    }
 }
