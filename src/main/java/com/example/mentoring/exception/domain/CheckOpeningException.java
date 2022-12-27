5dc6bdd4ff46f6a7799b420306c96a0cabdb7b90
pawoo0211 <pawoo0211@naver.com>
2022-12-27 20:12:32 +0900

    Add Component in CheckOpeningException #28

--- src/main/java/com/example/mentoring/exception/domain/CheckOpeningException.java
+++ src/main/java/com/example/mentoring/exception/domain/CheckOpeningException.java
@@ -1,10 +1,19 @@
 package com.example.mentoring.exception.domain;
 
-public class CheckOpeningException extends RuntimeException{
+import com.example.mentoring.constant.ExceptionKeyword;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CheckOpeningException extends MentoringException {
 
     private static final String MESSAGE = "주문 가능한 시간이 아닙니다.";
 
     public CheckOpeningException() {
         super(MESSAGE);
+    }
+
+    @Override
+    public String getKeyword() {
+        return ExceptionKeyword.CheckOpening.getKeyword();
     }
 }
