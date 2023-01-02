9e77bf3597623c1c75a443deb77d25520e3ac92b
박상권 <79737870+pawoo0211@users.noreply.github.com>
2023-01-02 23:06:20 +0900

    Delete ExceptionManager Class #29 (#30)

--- src/main/java/com/example/mentoring/exception/domain/MenuNotFoundException.java
+++ src/main/java/com/example/mentoring/exception/domain/MenuNotFoundException.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Component;
 @Component
 public class MenuNotFoundException extends MentoringException {
 
-    // log Message
     private static final String MESSAGE = "해당 메뉴는 존재하지 않는 메뉴입니다.";
 
     public MenuNotFoundException() {
